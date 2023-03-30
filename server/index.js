a6e24afb66c5a8e301d87b55a3b1de9e00e99aec
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-03-30 19:13:16 +0200

    connect server to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './mongodb/connect.js';
 
 dotenv.config();
 const app = express();
@@ -10,4 +11,14 @@ app.use(express.json({ limit: '50mb' }));
 app.get('/', (req, res) => res.send({ message: 'Welcome to real estate server' }));
 
 const port = process.env.SERVER_PORT || 8080;
-app.listen(port, () => console.log(`Server is running on PORT ${port}`));
+
+const startServer = async () => {
+	try {
+		connectDB(process.env.MONGODB_URL);
+		app.listen(port, () => console.log(`Server is running on PORT ${port}`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+startServer();
