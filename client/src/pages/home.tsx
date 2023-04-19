6d1913bb7e0f32cd700ce80a43744f5ddbb7e509
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-04-19 17:55:59 +0200

    add properties to home page

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -3,8 +3,21 @@ import { useList } from '@refinedev/core';
 import { ColorModeContext } from '../contexts/color-mode';
 import { PieChart, PropertyCard, PropertyReferrals, TopAgent, TotalRevenue } from 'components';
 import { Box, Stack, Typography } from '@mui/material';
+
 export const Home = () => {
 	const { mode } = useContext(ColorModeContext);
+	const { data, isLoading, isError } = useList({
+		resource: 'properties',
+		config: {
+			pagination: {
+				pageSize: 6,
+			},
+		},
+	});
+
+	const latestProperties = data?.data ?? [];
+	if (isLoading) return <div>Loading....</div>;
+	if (isError) return <div>Error</div>;
 	return (
 		<Box>
 			<Typography fontSize={25} fontWeight={700} color={mode === 'dark' ? '#fff' : '#11142d'}>
@@ -45,6 +58,35 @@ export const Home = () => {
 				<TotalRevenue mode={mode} />
 				<PropertyReferrals mode={mode} />
 			</Stack>
+
+			<Box
+				flex={1}
+				borderRadius='15px'
+				padding='20px'
+				bgcolor={mode === 'dark' ? '#ffffff14' : '#fff'}
+				display='flex'
+				flexDirection='column'
+				minWidth='100%'
+				mt='25px'
+			>
+				<Typography fontSize={18} fontWeight={600} color={mode === 'dark' ? '#EFEFEF' : '#11142D'}>
+					Latest Properties
+				</Typography>
+				<Box mt='20px' sx={{ width: '100%', display: 'flex', flexWrap: 'wrap', gap: 3 }}>
+					{latestProperties.map((property) => (
+						<PropertyCard
+							key={property._id}
+							id={property._id}
+							title={property.title}
+							price={property.price}
+							location={property.location}
+							photo={property.photo}
+							mode={mode}
+							colorCard='none'
+						/>
+					))}
+				</Box>
+			</Box>
 		</Box>
 	);
 };
